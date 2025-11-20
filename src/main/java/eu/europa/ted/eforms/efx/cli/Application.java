1e1efb1313ebd7dbb016762d66e0f93fb1d8141d
Ioannis Rosuochatzakis <ioannis.rousochatzakis@publications.europa.eu>
2025-11-20 01:43:20 +0100

    Refactor CLI structure, add banner, license and README

---
+++ src/main/java/eu/europa/ted/eforms/efx/cli/Application.java
@@ -0,0 +1,30 @@
+package eu.europa.ted.eforms.efx.cli;
+
+import picocli.CommandLine;
+import picocli.CommandLine.IExecutionExceptionHandler;
+import picocli.CommandLine.ParseResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        CommandLine cli = new CommandLine(new CliCommand());
+
+        cli.setExecutionExceptionHandler(new IExecutionExceptionHandler() {
+            @Override
+            public int handleExecutionException(Exception ex, CommandLine commandLine, ParseResult parseResult) {
+                logger.error("Error executing the application with arguments {}. Please see the logs.",
+                        Arrays.asList(args));
+                logger.debug("Exception thrown:", ex);
+                return 1;
+            }
+        });
+
+        int exitCode = cli.execute(args);
+        System.exit(exitCode);
+    }
+}
---
+++ src/main/java/eu/europa/ted/eforms/efx/cli/CliCommand.java
@@ -0,0 +1,67 @@
+package eu.europa.ted.eforms.efx.cli;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
+
+@Command(name = "efx-cli", mixinStandardHelpOptions = true, description = {
+        "             ██████                                  ████   ███ ",
+        "            ███▒▒███                                ▒▒███  ▒▒▒  ",
+        "  ██████   ▒███ ▒▒▒  █████ █████             ██████  ▒███  ████ ",
+        " ███▒▒███ ███████   ▒▒███ ▒▒███  ██████████ ███▒▒███ ▒███ ▒▒███ ",
+        "▒███████ ▒▒▒███▒     ▒▒▒█████▒  ▒▒▒▒▒▒▒▒▒▒ ▒███ ▒▒▒  ▒███  ▒███ ",
+        "▒███▒▒▒    ▒███       ███▒▒▒███            ▒███  ███ ▒███  ▒███ ",
+        "▒▒██████   █████     █████ █████           ▒▒██████  █████ █████",
+        " ▒▒▒▒▒▒   ▒▒▒▒▒     ▒▒▒▒▒ ▒▒▒▒▒             ▒▒▒▒▒▒  ▒▒▒▒▒ ▒▒▒▒▒ ",
+        "",
+        "Command Line Interface for the EFX Toolkit"
+}, versionProvider = CliCommand.ManifestVersionProvider.class)
+public class CliCommand implements Callable<Integer> {
+
+    @Option(names = { "-v", "--verbose" }, description = "Enable verbose output")
+    boolean verbose;
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Hello from EFX CLI!");
+        if (verbose) {
+            System.out.println("Verbose mode is on.");
+        }
+        return 0;
+    }
+
+    /**
+     * {@link picocli.CommandLine.IVersionProvider} implementation that returns
+     * version information from the
+     * jar file's {@code /META-INF/MANIFEST.MF} file.
+     */
+    static class ManifestVersionProvider implements picocli.CommandLine.IVersionProvider {
+        public String[] getVersion() throws Exception {
+            java.util.Enumeration<java.net.URL> resources = picocli.CommandLine.class.getClassLoader()
+                    .getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                java.net.URL url = resources.nextElement();
+                try {
+                    java.util.jar.Manifest manifest = new java.util.jar.Manifest(url.openStream());
+                    if (isApplicableManifest(manifest)) {
+                        java.util.jar.Attributes attr = manifest.getMainAttributes();
+                        return new String[] { get(attr, "Implementation-Title") + " version \""
+                                + get(attr, "Implementation-Version") + "\"" };
+                    }
+                } catch (java.io.IOException ex) {
+                    return new String[] { "Unable to read from " + url + ": " + ex };
+                }
+            }
+            return new String[0];
+        }
+
+        private boolean isApplicableManifest(java.util.jar.Manifest manifest) {
+            java.util.jar.Attributes attributes = manifest.getMainAttributes();
+            return "efx-cli".equals(get(attributes, "Implementation-Title"));
+        }
+
+        private static Object get(java.util.jar.Attributes attributes, String key) {
+            return attributes.get(new java.util.jar.Attributes.Name(key));
+        }
+    }
+}
